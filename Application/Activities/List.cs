b57c9260659b91006f3482e60eb74abd69052470
Rafael Gomes <maciel.rafael1@gmail.com>
2023-07-13 14:56:30 -0400

    configure backend for following and followers

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,10 +1,10 @@
 using MediatR;
-using Domain;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
 using Application.Core;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Application.Interfaces;
 
 namespace Application.Activities
 {
@@ -16,11 +16,13 @@ namespace Application.Activities
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
+            private readonly IUserAccessor _userAccessor;
 
-            public Handler(DataContext context, IMapper mapper)
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
             {
                 _context = context;
                 _mapper = mapper;
+                _userAccessor = userAccessor;
             }
 
             public async Task<Result<List<ActivityDto>>> Handle(
@@ -29,7 +31,10 @@ namespace Application.Activities
             )
             {
                 var activities = await _context.Activities
-                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+                    .ProjectTo<ActivityDto>(
+                        _mapper.ConfigurationProvider,
+                        new { currentUsername = _userAccessor.GetUsername() }
+                    )
                     .ToListAsync();
 
                 return Result<List<ActivityDto>>.Success(activities);
